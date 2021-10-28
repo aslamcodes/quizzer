388ff65a22f9e42ffa98b946c425e984863873be
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-10-28 15:40:52 +0530

    Topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Topbar } from "./components/common";
+import { Home } from "./components/pages";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="">
+      <Topbar />
+      <div className="mt-20">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/common/Topbar.tsx
+++ src/components/common/Topbar.tsx
@@ -1,5 +1,9 @@
 import { FC } from "react";
 
 export const Topbar: FC = () => {
-  return <div className=""></div>;
+  return (
+    <div className="fixed top-0 w-full flex justify-between items-center p-5 text-3xl text-white bg-blue-400">
+      Quizzer
+    </div>
+  );
 };
